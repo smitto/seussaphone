0f91f64a21f50e80ce1daac4fb266f8ee008ed3f
Nathan Smith <nathansforyou@gmail.com>
2020-05-07 01:39:55 -0400

    Vue updates, functional recording and playback system added

--- seussaphone.js
+++ seussaphone.js
@@ -1,10 +1,14 @@
 var note = Vue.component('note-item', {
+  template: '#note',
   data: function () {
     return {
       nowPlaying: false
     }
   },
-  props: ['note'],
+  props: {
+    note: Object,
+    isRecording: Boolean
+  },
   computed: {
     getPosition: function () {
       if (this.note.id % 2 != 0) {
@@ -15,24 +19,28 @@ var note = Vue.component('note-item', {
     }
   },
   methods: {
-    playNote: function () {
+    playNote: function (recording) {
+      if (this.isRecording) {
+        this.$emit('clicked', this.note.id)
+      }
       var file = 'sounds/' + this.note.instrument + '-' + this.note.pitch + '.wav'
       var sound = new Howl({ src: [file], html5: true });
       this.nowPlaying = true;
       sound.play();
     }
-  },
-  template: 
-    '<div v-bind:class="{ key: true, sharp: note.sharp, playing: nowPlaying }" \
-    v-bind:style="{ left: note.left + \'%\' }" \
-    v-bind:data-note="note.pitch" \
-    @click="playNote" \
-    @transitionend="nowPlaying = false"></div>'
+  }
 })
 
 var seussaphone = new Vue({
   el: '#seussaphone',
   data: {
+    isRecording: false,
+    isPlaying: false,
+    currentlyPlaying: null,
+    recordedNotes: [],
+    startTime: null,
+    totalTime: null,
+    loopTimeouts: [],
     noteList: [
       { id: 0, instrument: 'Bonkulon', pitch: 'C' },
       { id: 1, sharp: true, instrument: 'Bonkulon', pitch: 'Cs', left: 10 },
@@ -47,5 +55,46 @@ var seussaphone = new Vue({
       { id: 10, sharp: true, instrument: 'Bonkulon', pitch: 'As', left: 80 },
       { id: 11, instrument: 'Bonkulon', pitch: 'B' }
     ],
+  },
+  computed: {
+    recordedNotesPrintout: function () {
+      return JSON.stringify(this.recordedNotes);
+    }
+  },
+  methods: {
+    addToRecording: function (noteId) {
+      this.recordedNotes.push({ note: noteId, time: new Date().getTime() });
+    },
+    startRecording: function () {
+      this.isRecording = true;
+      this.startTime = new Date().getTime();
+
+      // stop old loop
+      window.clearInterval(this.loopInterval);
+      $.each(this.loopTimeouts, function(i, x) {
+        window.clearTimeout(x);
+      });
+    },
+    stopRecording: function () {
+      this.isRecording = false;
+      this.totalTime = new Date().getTime() - this.startTime;
+      for (var i=0, len=this.recordedNotes.length; i<len; i++) {
+        this.recordedNotes[i].time = this.recordedNotes[i].time - this.startTime;
+      }
+    },
+    clearRecording: function () {
+      this.isRecording = false;
+      this.recordedNotes = [];
+    },
+    playRecording: function () {
+      this.isRecording = false;
+      let loopTimeouts = [];
+      let refNotes = this.$refs.notes;
+      $.each(this.recordedNotes, function(i, x) {
+        loopTimeouts.push(window.setTimeout(function() {
+          refNotes[x.note].playNote();
+        }, x.time));
+      });
+    }
   }
 })
